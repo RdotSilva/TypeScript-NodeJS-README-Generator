fe30484714979f7854dcfb8d0e760e461c371c90
RdotSilva <RyandotSilva@gmail.com>
2022-02-23 19:21:04 +0000

    Add example for markdown

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 // TODO: Add npm package to read from command line
 
 // TODO: Create a list of questions to pass into that package
-    // TODO: Should have validation for questions (some can be optional)
+// TODO: Should have validation for questions (some can be optional)
 
 // TODO: Create a generateMarkdown util that will take in that list of questions and generate the markdown
 
 // TODO: Add main function that runs when you run the file
-    // TODO: Pass that list of questions into npm package and get the answers 
-    // TODO: Pass in the answers into generateMarkdown utility
-    // TODO: Get the returned markdown from generateMarkdown and write that to file (README.md)
+// TODO: Pass that list of questions into npm package and get the answers
+// TODO: Pass in the answers into generateMarkdown utility
+// TODO: Get the returned markdown from generateMarkdown and write that to file (README.md)
 
+const createMarkdown = require("../utils/createMarkdown");
 
+const markdown = createMarkdown("Cool");
 
-
-
+console.log(markdown);
