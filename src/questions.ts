ef9870f39c4f101ea4167f8f4b24c191f8c1468f
RdotSilva <RyandotSilva@gmail.com>
2022-02-24 17:42:49 +0000

    Add starter questions

---
+++ src/questions.ts
@@ -0,0 +1,29 @@
+/**
+ * These are the questions that will we will prompt in command line to create the README.md
+ */
+export const questions = [
+  {
+    type: "input",
+    message: "Project Title",
+    name: "title",
+    default: "Project Title",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Project Title Required");
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    message: "Project Description",
+    name: "description",
+    default: "Project Description",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Project Description Required");
+      }
+      return true;
+    },
+  },
+];
