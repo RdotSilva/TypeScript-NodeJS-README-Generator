cf055b6fdcb00d8eb4ab66f20134619ab5ef87fe
RdotSilva <RyandotSilva@gmail.com>
2022-02-26 06:54:47 -0500

    Remove TODOs and clean up imports

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,15 @@
-// TODO: Add npm package to read from command line
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
+const readmeOutputPath = path.join(__dirname, "../output/README.md");
 
-// TODO: Create a list of questions to pass into that package
-// TODO: Should have validation for questions (some can be optional)
 import { questions } from "./questions";
-
-// TODO: Create a generateMarkdown util that will take in that list of questions and generate the markdown
 import { createMarkdown } from "../utils/createMarkdown";
-
-// TODO: Add main function that runs when you run the file
-// TODO: Pass that list of questions into npm package and get the answers
-// TODO: Pass in the answers into generateMarkdown utility
-// TODO: Get the returned markdown from generateMarkdown and write that to file (README.md)
 
 async function main() {
   try {
     const userResponses = await inquirer.prompt(questions);
     const markdown = createMarkdown(userResponses);
-    const readmeOutputPath = path.join(__dirname, "../output/README.md");
     fs.writeFileSync(readmeOutputPath, markdown);
   } catch (error) {
     console.log(error);
