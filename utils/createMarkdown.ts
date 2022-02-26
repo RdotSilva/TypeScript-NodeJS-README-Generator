4151b2ee7f3432e9156fdd1ece2ede6b2ce55385
RdotSilva <RyandotSilva@gmail.com>
2022-02-26 07:02:33 -0500

    Add markdownData interface

--- utils/createMarkdown.ts
+++ utils/createMarkdown.ts
@@ -1,9 +1,19 @@
+interface markdownData {
+  title: string;
+  description: string;
+  installation: string;
+}
+
 /**
  * Create markdown from data
  * @param data The data you want to use to create markdown
  * @returns Markdown string
  */
-export const createMarkdown = ({title, description, installation}): String => {
+export const createMarkdown = ({
+  title,
+  description,
+  installation,
+}: markdownData): String => {
   return `# ${title}
   # ${description}
   # ${installation}
