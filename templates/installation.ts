83c03018ce13fcc3d3f6b98432a8f25ff8111f24
RdotSilva <RyandotSilva@gmail.com>
2022-03-01 21:19:43 +0000

    Update pythonInstallation template

--- templates/installation.ts
+++ templates/installation.ts
@@ -4,5 +4,8 @@ npm install
 \`\`\`
 `;
 
-export const pythonInstallation = `TODO: Add Python instructions
+export const pythonInstallation = `1. Install dependencies in main project folder
+\`\`\`
+pip install -r requirements.txt
+\`\`\`
 `;
