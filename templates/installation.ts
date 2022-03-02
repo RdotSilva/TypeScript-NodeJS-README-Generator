8f06c1ac7559846cab5908476736d3a85b89a31f
RdotSilva <RyandotSilva@gmail.com>
2022-03-02 19:02:20 +0000

    Update templates to include line break

--- templates/installation.ts
+++ templates/installation.ts
@@ -1,22 +1,26 @@
 export const javaScriptInstallation = `1. Install dependencies in main project folder
+
 \`\`\`
 npm install
 \`\`\`
 `;
 
 export const pythonInstallation = `1. Install dependencies in main project folder
+
 \`\`\`
 pip install -r requirements.txt
 \`\`\`
 `;
 
 export const mavenInstallation = `1. Install dependencies in main project folder
+
 \`\`\`
 mvn install
 \`\`\`
 `;
 
 export const gradleInstallation = `1. Install dependencies in main project folder
+
 \`\`\`
 ./gradlew build --refresh-dependencies
 \`\`\`
